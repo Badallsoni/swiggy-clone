d60f3872a095eec969ad7e83116e3bcbb8d66ee5
Badal Kumar Soni <kumarbadal4080@gmail.com>
2026-08-02 19:34:04 +0530

    Make header responsive across devices

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,49 +1,225 @@
 import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
-    <header className="font-serif bg-[#ff5200]">
-      <div className="flex justify-between container mx-auto py-8">
+    <header className="font-serif bg-[#ff5200] overflow-hidden">
+
+      {/* Navbar */}
+      <div className="flex justify-between items-center container mx-auto px-4 md:px-8 py-5 md:py-8">
+
         <img
-          className="w-40 h-12"
+          className="w-28 md:w-40 h-auto"
           src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/static-assets/images/swiggy_logo_white.png"
+          alt="Swiggy"
         />
-        <div className="font-serif text-white text-base font-bold flex gap-15 items-center">
-          <a target="_blank" href="https://www.swiggy.com/corporate/">Swiggy Corporate</a>
-           <a target="_blank" href="https://partner.swiggy.com/food/login">Partner with Us</a>
-            <a target="_blank" className="border border-white rounded-2xl py-3 px-4">Get The App</a>
-             <a target="_blank" className="border border-black rounded-2xl bg-black py-3 px-4">Sign in</a>
+
+        <div className="font-serif text-white text-base font-bold flex gap-4 lg:gap-15 items-center">
+
+          <a
+            className="hidden lg:block"
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.swiggy.com/corporate/"
+          >
+            Swiggy Corporate
+          </a>
+
+          <a
+            className="hidden lg:block"
+            target="_blank"
+            rel="noreferrer"
+            href="https://partner.swiggy.com/food/login"
+          >
+            Partner with Us
+          </a>
+
+          <a
+            className="hidden md:block border border-white rounded-2xl py-2 md:py-3 px-3 md:px-4"
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.swiggy.com/"
+          >
+            Get The App
+          </a>
+
+          <a
+            className="border border-black rounded-2xl bg-black py-2 md:py-3 px-3 md:px-4"
+            href="https://www.swiggy.com/"
+          >
+            Sign in
+          </a>
+
         </div>
-        
       </div>
 
-      <div className="pt-16 pb-8 relative" >
-        <img className="h-90 w-80 absolute top-0 left-0"  src="https://pngimg.com/uploads/burger_sandwich/burger_sandwich_PNG4135.png"/>
-        <img className="h-100 w-80 absolute top-0 right-0 "   src="https://static.vecteezy.com/system/resources/previews/021/311/734/original/pizza-transparent-background-png.png"></img>
-        <div className="text-5xl text-white font-bold max-w-[60%] container mx-auto text-center">Order Food and Groceries. Discover best restaurents.Swiggy it. </div>
-        <div className="max-w-[70%] container mx-auto flex gap-10 justify-baseline mr-20 mt-4 ">
-        <input placeholder="Delhi,India" className="max-w-[40%] bg-white w-[40%] text-xl px-6 py-2 rounded-2xl "></input>
-        <input placeholder="Search for restaurants and items for more" className=" bg-white w-[55%]  text-xl px-6 py-2 mr-50 rounded-2xl" ></input>   
 
+      {/* Hero Section */}
+      <div className="pt-10 md:pt-16 pb-8 relative">
+
+        {/* Burger */}
+        <img
+          className="
+            hidden md:block
+            h-60 lg:h-90
+            w-52 lg:w-80
+            object-contain
+            absolute top-0 left-0
+          "
+          src="https://pngimg.com/uploads/burger_sandwich/burger_sandwich_PNG4135.png"
+          alt=""
+        />
+
+        {/* Pizza */}
+        <img
+          className="
+            hidden md:block
+            h-64 lg:h-100
+            w-52 lg:w-80
+            object-contain
+            absolute top-0 right-0
+          "
+          src="https://static.vecteezy.com/system/resources/previews/021/311/734/original/pizza-transparent-background-png.png"
+          alt=""
+        />
+
+
+        {/* Heading */}
+        <div
+          className="
+            text-3xl
+            md:text-4xl
+            lg:text-5xl
+            text-white
+            font-bold
+            w-[90%]
+            md:max-w-[60%]
+            mx-auto
+            text-center
+            relative
+            z-10
+          "
+        >
+          Order Food and Groceries. Discover best restaurants. Swiggy it.
         </div>
 
-      </div>
-      <div className="max-w-[80%] container mx-auto flex mt-30 mr-40 gap-6 py-6" >
-       <Link to="/restaurant">
-          <img className="rounded-4xl"  src="https://cdn.growthjockey.com/blogs/swiggy-mall-offering-integrates-with-instamart-69b3b2-480x359.jpeg"></img>
-       </Link>
 
-         <a href="https://www.swiggy.com/restaurants" >
-          <img className="rounded-4xl w-120 h-53"  src="https://mir-s3-cdn-cf.behance.net/projects/404/1d31dc85845197.Y3JvcCw4NjIsNjc1LDE0Myww.jpg"></img>
+        {/* Search Inputs */}
+        <div
+          className="
+            w-[90%]
+            md:max-w-[70%]
+            mx-auto
+            flex
+            flex-col
+            md:flex-row
+            gap-4
+            md:gap-10
+            mt-6
+            relative
+            z-10
+          "
+        >
+
+          <input
+            placeholder="Delhi, India"
+            className="
+              bg-white
+              w-full
+              md:w-[40%]
+              text-base
+              md:text-xl
+              px-6
+              py-3
+              rounded-2xl
+              outline-none
+            "
+          />
+
+          <input
+            placeholder="Search for restaurants and items"
+            className="
+              bg-white
+              w-full
+              md:w-[60%]
+              text-base
+              md:text-xl
+              px-6
+              py-3
+              rounded-2xl
+              outline-none
+            "
+          />
+
+        </div>
+      </div>
+
+
+      {/* Promotional Cards */}
+      <div
+        className="
+          w-[90%]
+          max-w-6xl
+          mx-auto
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          lg:grid-cols-4
+          gap-6
+          mt-10
+          md:mt-20
+          py-6
+        "
+      >
+
+        <Link to="/restaurant">
+          <img
+            className="w-full aspect-[4/3] object-cover rounded-4xl"
+            src="https://cdn.growthjockey.com/blogs/swiggy-mall-offering-integrates-with-instamart-69b3b2-480x359.jpeg"
+            alt="Swiggy restaurants"
+          />
+        </Link>
+
+
+        <a
+          href="https://www.swiggy.com/restaurants"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="w-full aspect-[4/3] object-cover rounded-4xl"
+            src="https://mir-s3-cdn-cf.behance.net/projects/404/1d31dc85845197.Y3JvcCw4NjIsNjc1LDE0Myww.jpg"
+            alt="Swiggy"
+          />
         </a>
 
-         <a href="https://www.swiggy.com/restaurants" >
-          <img className="rounded-4xl w-120 h-53  "  src="https://www.entertales.com/wp-content/uploads/2024/11/Swiggy-IPO-Details.jpg "></img>
+
+        <a
+          href="https://www.swiggy.com/restaurants"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="w-full aspect-[4/3] object-cover rounded-4xl"
+            src="https://www.entertales.com/wp-content/uploads/2024/11/Swiggy-IPO-Details.jpg"
+            alt="Swiggy"
+          />
         </a>
-       
- <a href="https://www.swiggy.com/restaurants" >
-          <img  className="rounded-4xl w-120 h-53" src="https://images.goodreturns.in/img/2025/01/swiggy-1736326958.jpg"></img>
+
+
+        <a
+          href="https://www.swiggy.com/restaurants"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="w-full aspect-[4/3] object-cover rounded-4xl"
+            src="https://images.goodreturns.in/img/2025/01/swiggy-1736326958.jpg"
+            alt="Swiggy"
+          />
         </a>
+
       </div>
+
     </header>
   );
 }
